373ea2d02d09da1d4a8810c40d27156c446c3ce1
TrotskyuYurii <tro2001@gmail.com>
2024-05-23 23:18:31 +0300

    bed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import express from 'express';
+import pino, { transport } from 'pino';
 
-export const app = express();
+const app = express();
+// app.use(pino(
+//   {
+//     transport:{
+//       target: 'pino-pretty',}}
+
+// )),(req, res) => {
+//   console.log(req.body);
+// };
+
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello, World!'
+  });
+});
+
 
 const PORT = 3000;
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
