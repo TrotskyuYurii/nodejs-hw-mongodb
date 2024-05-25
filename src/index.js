75ac5a95d14bc764dbd1e95ca117610980dc659a
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-05-25 13:26:32 +0300

    add env

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
-import express from 'express';
-import pino, { transport } from 'pino';
-
-const app = express();
-// app.use(pino(
-//   {
-//     transport:{
-//       target: 'pino-pretty',}}
-
-// )),(req, res) => {
-//   console.log(req.body);
-// };
+import {setupServer} from './server.js';
 
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello, World!'
-  });
-});
-
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+setupServer();
--- src/server.js
+++ src/server.js
@@ -1,9 +1,34 @@
-// import express from 'express';
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from '../utils/env.js';
+import { ENV_VARS } from '../const/const.js';
 
-// export const app = express();
 
-// const PORT = 3000;
+export function setupServer() {
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+    const app = express();
+    // app.use(pino(
+    //   {
+    //     transport:{
+    //       target: 'pino-pretty',}}
+    
+    // )),(req, res) => {
+    //   console.log(req.body);
+    // };
+    
+    
+    app.get('/', (req, res) => {
+      res.json({
+        message: 'Hello, World!'
+      });
+    });
+    
+    
+    const PORT = env(ENV_VARS.PORT, 3000);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+}
+
